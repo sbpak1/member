01a8981fe92dd0ad45da563cb131b318f0e0d90d
sbpak1 <sbpak1@gmail.com>
2025-08-13 17:50:10 +0900

    1. 멤버 데이터 뿌려주기 완성 2. member 엔티티를 DTO로 처리 완료 3. DTO 변환 시 stream의 맵으로 처리함

--- src/main/java/com/my/member/service/MemberService.java
+++ src/main/java/com/my/member/service/MemberService.java
@@ -1,9 +1,11 @@
 package com.my.member.service;
 
+import com.my.member.dto.MemberDto;
 import com.my.member.entity.Member;
 import com.my.member.repository.MemberRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -16,9 +18,28 @@ public class MemberService {
         this.repository = repository;
     }
 
-    public List<Member> getAllList() {
+    public List<MemberDto> getAllList() {
         List<Member> memberList = repository.findAll();
         System.out.println(memberList);
-        return memberList;
+        // 비어있는 DTO List 만들기
+//        List<MemberDto> dtoList = new ArrayList<>();
+        // Entity List를 DTO List로 변환
+//        for (int i = 0; i < memberList.size(); i++) {
+//            // 리스트에 있는 엔티티를 하나씩 읽어서
+//            // Dto에 담는다.
+//            MemberDto dto = new MemberDto();
+//            dto.setId(memberList.get(i).getId());
+//            dto.setName(memberList.get(i).getName());
+//            dto.setAge(memberList.get(i).getAge());
+//            dto.setAddress(memberList.get(i).getAddress());
+//            dtoList.add(dto);
+//        }
+
+        // fromMemberEntity 메서드로 작업하기
+        return memberList
+                .stream()
+                .map(x -> MemberDto.fromMemberEntity(x))
+                .toList();
+//        return dtoList;
     }
 }
